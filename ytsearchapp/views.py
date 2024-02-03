a7ca8b6653eb5097eab1b3ff1530611c6837b64c
webmaster0109 <itdepartmentvfo@gmail.com>
2024-02-03 19:13:23 +0530

    first commit

---
+++ ytsearchapp/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404, redirect
+import time
+from .youtube import *
+from dateutil import parser
+from django.utils.safestring import mark_safe
+from django.template.defaultfilters import urlize
+# Create your views here.
+
+def home(request):
+    search_query = request.GET.get("search_query")
+    start_time = time.time()
+    video_class = YoutubeVideoSearch(search_query, max_limit=200)
+    videos = video_class.search_youtube_videos()
+    next_videos = []
+    for _ in range(3):  # Fetch results from the next three pages
+        next_videos = video_class.search_more_videos()
+        next_videos.extend(next_videos)
+    end_time = time.time()
+    
+    suggestions = search_video_suggestions(search_query)
+
+    if videos:
+        elapsed_time_second = end_time - start_time
+        video_length = len(videos + next_videos)
+        videos_content = videos
+        next_videos = next_videos
+
+
+    return render(
+        request,
+        "index.html",
+        context={
+            'time': f'{elapsed_time_second:.2f}',
+            'length': video_length, 
+            'videos': videos_content,
+            'next_videos':next_videos,
+            'suggestions': suggestions
+        })
+
+def watch_video(request):
+
+    video_id = request.GET.get('video')
+
+    get_video = get_video_detail(video_id)
+
+    get_comments = get_video_comments(video_id)
+
+    if get_video:
+        views_int = get_video['viewCount']['text']
+        views = f'{int(views_int):,} views'
+
+        description = get_video['description']
+        safe_description = mark_safe(urlize(description))
+
+        # Your input string
+        datetime_str = get_video['publishDate']
+        datetime_object = parser.parse(datetime_str)
+        formatted_datetime = datetime_object.strftime("%d %B %Y")
+    
+    if get_comments:
+        comment_length = len(get_comments)
+        comments = get_comments
+
+    return render(
+        request,
+        "video.html",
+        context={
+            'video': get_video,
+            'comments': comments,
+            'views': views,
+            'description': safe_description,
+            'publishedDate': formatted_datetime,
+            'length': comment_length
+        })
---
+++ ytsearchapp/youtube.py
@@ -0,0 +1,38 @@
+from youtubesearchpython import *
+
+def recent_youtube_videos(query, max_limit=100):
+    videos_search = CustomSearch(query, VideoSortOrder.uploadDate, limit=max_limit)
+    results = videos_search.result()
+    return results["result"]
+
+class YoutubeVideoSearch:
+    # constructor
+    def __init__(self, query, max_limit=100):
+        self.search = VideosSearch(query, limit=max_limit)
+
+    def search_youtube_videos(self):
+        return self.search.result()["result"]
+
+    def search_more_videos(self):
+        self.search.next()
+        return self.search_youtube_videos()
+
+def search_youtube_playlist(playlist):
+    playlistsSearch = PlaylistsSearch(playlist)
+    playlists = playlistsSearch.result()
+    return playlists["result"]
+
+def get_playlist_videos(query):
+    playlistVideos = Playlist.getVideos(query)
+    return playlistVideos
+
+def get_video_detail(query):
+    videoInfo = Video.getInfo(query)
+    return videoInfo
+
+def get_video_comments(video_id):
+    return Comments.get(video_id)['result']
+
+def search_video_suggestions(query):
+    suggestions = Suggestions(language='en', region='US')
+    return suggestions.get(query)['result']
