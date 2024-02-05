1ff64518ce58c2279f83a6cc6e02f2a3aa29338e
webmaster0109 <itdepartmentvfo@gmail.com>
2024-02-05 18:26:13 +0530

    app

--- ytsearchapp/urls.py
+++ ytsearchapp/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', home, name="home"),
     path('watch', watch_video, name="watch_video"),
+    path('playlist', playlist_videos, name="playlist")
 ]
--- ytsearchapp/views.py
+++ ytsearchapp/views.py
@@ -23,13 +23,21 @@ def home(request):
     suggestions = search_video_suggestions(search_query)
 
     # playlist
-    
+    start_time_playlist = time.time()
+    playlist_class = YoutubePlaylistSearch(search_query)
+    playlist = playlist_class.search_youtube_playlist()
+    end_time_playlist = time.time()
 
-    if videos:
+    if videos or playlist:
         elapsed_time_second = end_time - start_time
         video_length = len(videos + next_videos)
         videos_content = videos
         next_videos = next_videos
+
+        elapsed_time_second_playlist = end_time_playlist - start_time_playlist
+        elapsed_time_second_playlist = f'{elapsed_time_second_playlist:.2f}'
+        playlist_length = len(playlist)
+        playlist_content = playlist
 
 
     return render(
@@ -37,11 +45,14 @@ def home(request):
         "index.html",
         context={
             'time': f'{elapsed_time_second:.2f}',
+            'time_playlist': elapsed_time_second_playlist,
             'length': video_length, 
             'videos': videos_content,
             'next_videos':next_videos,
             'suggestions': suggestions,
-            'option_selected': option_selected
+            'option_selected': option_selected,
+            'playlist_length': playlist_length,
+            'playlists': playlist_content
         })
 
 def watch_video(request):
@@ -79,3 +90,22 @@ def watch_video(request):
             'publishedDate': formatted_datetime,
             'length': comment_length
         })
+
+def playlist_videos(request):
+
+    playlist_id = request.GET.get('list')
+    start_time = time.time()
+    playlist_link = f'https://www.youtube.com/playlist?list={playlist_id}'
+    playlist_video = get_playlist_videos(playlist_link)['videos']
+    end_time = time.time()
+
+    if playlist_video:
+        elapsed_time_seconds = end_time - start_time
+        videos_length = len(playlist_video)
+        playlist_videos = playlist_video
+
+    return render(request, "playlist.html", context={
+        'time': f'{elapsed_time_seconds:.2f}',
+        'length': videos_length,
+        'playlists': playlist_videos
+    })
